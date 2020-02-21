b60a91cb4559b2bf8de35edbad460a55d6ccb50f
Pierre Ph@rel <edo.tensei095@gmail.com>
2020-02-21 20:27:54 +0100

    [land] random frames drawing

--- src/land.rb
+++ src/land.rb
@@ -4,24 +4,27 @@ require "sdl"
 class Land
 	def initialize(window)
 		@window = window
-		@images = [SDL::Surface.load("../assets/land_0.png"), SDL::Surface.load("../assets/land_1.png")]
+		@sprite = [SDL::Surface.load("../assets/land_0.png"), SDL::Surface.load("../assets/land_1.png")]
 		@speed = 0.25
 		@x = [0, 0]
-		@sprites = [0, 1]
-		@y = (@window.h - @images[0].h) - 10
+		@frames = [0, 1]
+		@y = (@window.h - @sprite[0].h) - 10
 	end
 
 	def animation
 		@x[0] -= @speed
-		SDL::Surface.blit(@images[sprites[0]], 0, 0, @images[0].w, @images[0].h, @window, @x[0], @y)
+		SDL::Surface.blit(@sprite[@frames[0]], 0, 0, @sprite[0].w, @sprite[0].h, @window, @x[0], @y)
 		if @x[0] <= 0
-			@x[1] = @x[0] + @images[0].w 
+			@x[1] = @x[0] + @sprite[0].w 
 		else
 			@x[1] -= @speed
 		end
 
-		SDL::Surface.blit(@images[sprites[1]], 0, 0, @images[0].w, @images[0].h, @window, @x[1], @y)
-		@x[0] = @x[1] + @images[1].w if @x[1] <= 0
+		SDL::Surface.blit(@sprite[@frames[1]], 0, 0, @sprite[0].w, @sprite[0].h, @window, @x[1], @y)
+		if @x[1] <= 0
+			@frames[0] = rand(0..1) if @x[1] == 0
+			@x[0] = @x[1] + @sprite[1].w 
+		end
 	end
 
 	def draw
